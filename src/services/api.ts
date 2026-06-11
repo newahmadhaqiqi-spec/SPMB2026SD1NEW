258a6c3b522a2c3d5ae611d03d1c894411d498d4
newahmadhaqiqi-spec <newahmadhaqiqi@gmail.com>
2026-06-11 14:34:05 +0700

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 // Service to interact with Google Apps Script Backend
 
 // To use the real backend, replace this URL with your deployed Google Apps Script Web App URL
-const GAS_WEB_APP_URL = ""; 
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwFdub6953PsUWmGFQLwLnBwS26MYRIYdAee2J9VwgPlhighVPjcRROuYgRUN7eTX4O-A/exec"; 
 
 export interface FormField {
   id: string;
